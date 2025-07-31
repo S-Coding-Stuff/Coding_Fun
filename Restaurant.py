c953bda0a200f337a757a15ae60d5dea6021b6f6
40329813 <sgrant25@qub.ac.uk>
2025-07-31 09:55:55 +0100

    Adding Restaurant_Menu.py

--- Restaurant.py
+++ Restaurant.py
@@ -18,6 +18,7 @@ class Restaurant:
     def add_number_served(self):
         self.number_served += 1
 
+# Ice Cream Stand class inherits from Restaurant
 class IceCreamStand(Restaurant):
     def __init__(self, restaurant_name, cuisine_type, number_served=0):
         super().__init__(restaurant_name, cuisine_type, number_served)
@@ -25,36 +26,3 @@ class IceCreamStand(Restaurant):
 
     def print_flavours(self):
         print(flavour for flavour in self.flavours)
-
-
-restaurant1 = Restaurant('McDonalds', 'American')
-restaurant2 = Restaurant('Wagamama', 'Japanese')
-restaurant3 = Restaurant('The Dirty Onion', 'Irish')
-ice_cream_stand = IceCreamStand('Ice Cream Shop', 'Ice Cream')
-
-def main_menu(restaurant):
-    print(f'Welcome to {restaurant.restaurant_name}!\n'
-          f'1. Describe Restaurant\n'
-          f'2. Set Number of People Served\n'
-          f'3. Add Person Served\n'
-          f'4. Exit')
-
-    while True:
-        choice = input("Enter your choice (1-4):")
-
-        if choice == '1':
-            restaurant.describe_restaurant()
-        elif choice == '2':
-            restaurant.set_number_served()
-        elif choice == '3':
-            restaurant.add_number_served()
-        elif choice == '4':
-            print('Goodbye!')
-            break
-        else:
-            print('Invalid Choice, Try Again:')
-
-        print(f'[{restaurant.restaurant_name}] - {restaurant.number_served} People Served Today')
-
-if __name__ == "__main__":
-    main_menu(ice_cream_stand)
---
+++ Restaurant_Menu.py
@@ -0,0 +1,33 @@
+from Restaurant import *
+
+restaurant1 = Restaurant('McDonalds', 'American')
+restaurant2 = Restaurant('Wagamama', 'Japanese')
+restaurant3 = Restaurant('The Dirty Onion', 'Irish')
+ice_cream_stand = IceCreamStand('Ice Cream Shop', 'Ice Cream')
+
+def main_menu(restaurant):
+    print(f'Welcome to {restaurant.restaurant_name}!\n'
+          f'1. Describe Restaurant\n'
+          f'2. Set Number of People Served\n'
+          f'3. Add Person Served\n'
+          f'4. Exit')
+
+    while True:
+        choice = input("Enter your choice (1-4):")
+
+        if choice == '1':
+            restaurant.describe_restaurant()
+        elif choice == '2':
+            restaurant.set_number_served()
+        elif choice == '3':
+            restaurant.add_number_served()
+        elif choice == '4':
+            print('Goodbye!')
+            break
+        else:
+            print('Invalid Choice, Try Again:')
+
+        print(f'[{restaurant.restaurant_name}] - {restaurant.number_served} People Served Today')
+
+if __name__ == "__main__":
+    main_menu(ice_cream_stand)
